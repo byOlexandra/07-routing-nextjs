d27846b2489371ad1502601826a2d20081d38df7
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-02-13 21:57:11 +0200

    Change tag a to Link

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import css from './SidebarNotes.module.css'
 
 export default function SidebarNotes() {
@@ -5,14 +6,14 @@ export default function SidebarNotes() {
         <ul className={css.menuList}>
             {/* список тегів */}
             <li className={css.menuItem}>
-                <a href={`/notes/filter/all`} className={css.menuLink}>
+                <Link href={`/notes/filter/all`} className={css.menuLink}>
                     All notes
-                </a>
+                </Link>
             </li>
             <li className={css.menuItem}>
-                <a href={`url до сторінки за відповідним тегом`} className={css.menuLink}>
+                <Link href={`url до сторінки за відповідним тегом`} className={css.menuLink}>
                     Назва тегу
-                </a>
+                </Link>
             </li>
         </ul>
 
